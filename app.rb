6dca462a296f5e26de38c8c99888d3b035274b32
alceo <alceo@alceo.ru>
2016-04-29 11:59:42 +0300

    valid activerecord

--- app.rb
+++ app.rb
@@ -7,7 +7,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-
+	validates :name, :presence => { :message => "Name cannot be blank" }
+	validates :phone, :presence => { :message => "Phone cannot be blank" }
+	validates :datestamp, :presence => { :message => "Date cannot be blank" }
+	validates :color, :presence => { :message => "Color cannot be blank" }
 end
 
 class Barber < ActiveRecord::Base
@@ -22,16 +25,19 @@ end
 
 get '/visit' do
 	@barbers = Barber.order "created_at DESC"
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
-
-erb "SPASIBO!"
-
+	@c = Client.new params[:client]
+	if	@c.save
+		erb "SPASIBO!"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit
+	end
 	#redirect to "/"
 
 end
